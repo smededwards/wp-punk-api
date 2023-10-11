856783f7f70b291d8adfa4748cedfb3e92b31f97
Michael Edwards <smededwards@gmail.com>
2023-10-11 13:45:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WP Punk API
+
+WordPress Plugin that connects to the Punk API, and imports the data into a Custom Post Type called Beers
---
+++ wp-punk-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP Punk API
+ * Plugin URI:  https://github.com/smededwards/wp-punk-api
+ * Description: A plugin that connects to the Punk API, and imports the data into a Custom Post Type called Beers
+ * Author:      Michael Edwards
+ * Author URI:  https://smededwards.com
+ * Text Domain: wp-punk-api
+ * Domain Path: /languages
+ * Version:     0.0.1
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
