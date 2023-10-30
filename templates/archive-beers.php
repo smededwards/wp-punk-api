09561b4ad65c022c9039581f6d17b05d49905359
Michael Edwards <smededwards@gmail.com>
2023-10-30 12:00:42 +0000

    feat: add featured image to beer block
    * Adding featured image to beer block
    * Conditional to the archive template

--- templates/archive-beers.php
+++ templates/archive-beers.php
@@ -17,7 +17,9 @@ get_header();
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<a href="<?php the_permalink(); ?>">
 					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-					<img src="<?= esc_url( get_the_post_thumbnail_url( $post->ID, 'medium' ) ); ?>" alt="<?php the_title(); ?>">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<img src="<?= esc_url( get_the_post_thumbnail_url( $post->ID, 'medium' ) ); ?>" alt="<?php the_title(); ?>">
+					<?php endif; ?>
 				</a>
 			</article>
 		<?php endwhile; ?>
